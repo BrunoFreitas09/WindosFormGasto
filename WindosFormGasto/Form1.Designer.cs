a97783a3ac30d0611d00210860d160105d0ed016
Usuario <Usuario@CPD02-M04>
2024-08-06 11:34:22 -0300

    Adicionar arquivos de projeto.

---
+++ WindosFormGasto/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindosFormGasto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txtRenda = new System.Windows.Forms.TextBox();
+            this.txtEnergia = new System.Windows.Forms.TextBox();
+            this.txtAgua = new System.Windows.Forms.TextBox();
+            this.txtInter = new System.Windows.Forms.TextBox();
+            this.txtAlimento = new System.Windows.Forms.TextBox();
+            this.txtOutros = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.lblGastos = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 47);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Energia Elétrica";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(10, 93);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(32, 13);
+            this.label12.TabIndex = 2;
+            this.label12.Text = "Água";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(6, 190);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(65, 13);
+            this.label13.TabIndex = 3;
+            this.label13.Text = "Alimentação";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(6, 147);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(98, 13);
+            this.label14.TabIndex = 4;
+            this.label14.Text = "Tv/Celular/Internet";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(10, 239);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(72, 13);
+            this.label15.TabIndex = 5;
+            this.label15.Text = "Outros gastos";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(136, 66);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(74, 13);
+            this.label16.TabIndex = 6;
+            this.label16.Text = "Renda familiar";
+            // 
+            // txtRenda
+            // 
+            this.txtRenda.Location = new System.Drawing.Point(229, 59);
+            this.txtRenda.Name = "txtRenda";
+            this.txtRenda.Size = new System.Drawing.Size(237, 20);
+            this.txtRenda.TabIndex = 8;
+            // 
+            // txtEnergia
+            // 
+            this.txtEnergia.Location = new System.Drawing.Point(117, 44);
+            this.txtEnergia.Name = "txtEnergia";
+            this.txtEnergia.Size = new System.Drawing.Size(100, 20);
+            this.txtEnergia.TabIndex = 9;
+            // 
+            // txtAgua
+            // 
+            this.txtAgua.Location = new System.Drawing.Point(117, 90);
+            this.txtAgua.Name = "txtAgua";
+            this.txtAgua.Size = new System.Drawing.Size(100, 20);
+            this.txtAgua.TabIndex = 10;
+            // 
+            // txtInter
+            // 
+            this.txtInter.Location = new System.Drawing.Point(117, 144);
+            this.txtInter.Name = "txtInter";
+            this.txtInter.Size = new System.Drawing.Size(100, 20);
+            this.txtInter.TabIndex = 11;
+            // 
+            // txtAlimento
+            // 
+            this.txtAlimento.Location = new System.Drawing.Point(117, 190);
+            this.txtAlimento.Name = "txtAlimento";
+            this.txtAlimento.Size = new System.Drawing.Size(100, 20);
+            this.txtAlimento.TabIndex = 12;
+            // 
+            // txtOutros
+            // 
+            this.txtOutros.Location = new System.Drawing.Point(117, 236);
+            this.txtOutros.Name = "txtOutros";
+            this.txtOutros.Size = new System.Drawing.Size(100, 20);
+            this.txtOutros.TabIndex = 13;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(229, 397);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Calcular";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(531, 397);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 16;
+            this.button.Text = "Fechar";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(373, 397);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Novo Calculo";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(163, 464);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(80, 13);
+            this.label18.TabIndex = 18;
+            this.label18.Text = "Total de gastos";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(163, 517);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(34, 13);
+            this.label19.TabIndex = 19;
+            this.label19.Text = "Saldo";
+            // 
+            // lblGastos
+            // 
+            this.lblGastos.AutoSize = true;
+            this.lblGastos.Location = new System.Drawing.Point(298, 464);
+            this.lblGastos.Name = "lblGastos";
+            this.lblGastos.Size = new System.Drawing.Size(13, 13);
+            this.lblGastos.TabIndex = 20;
+            this.lblGastos.Text = "0";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Location = new System.Drawing.Point(298, 517);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(13, 13);
+            this.lblSaldo.TabIndex = 21;
+            this.lblSaldo.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtOutros);
+            this.groupBox1.Controls.Add(this.txtAlimento);
+            this.groupBox1.Controls.Add(this.txtInter);
+            this.groupBox1.Controls.Add(this.txtAgua);
+            this.groupBox1.Controls.Add(this.txtEnergia);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Location = new System.Drawing.Point(137, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 275);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gastos";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1222, 664);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.lblGastos);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtRenda);
+            this.Controls.Add(this.label16);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox txtRenda;
+        private System.Windows.Forms.TextBox txtEnergia;
+        private System.Windows.Forms.TextBox txtAgua;
+        private System.Windows.Forms.TextBox txtInter;
+        private System.Windows.Forms.TextBox txtAlimento;
+        private System.Windows.Forms.TextBox txtOutros;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label lblGastos;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindosFormGasto/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindosFormGasto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //entrada 
+            double renda = double.Parse(txtRenda.Text);
+
+            double energia = double.Parse(txtEnergia.Text);
+            double agua = double.Parse(txtAgua.Text);
+            double inter = double.Parse(txtInter.Text);
+            double alimento = double.Parse(txtAlimento.Text);
+            double outros = double.Parse(txtOutros.Text);
+
+            //processamento 
+            double gastos = energia + agua + inter + alimento + outros;
+
+            double saldo = renda - gastos; 
+
+            //Saída
+            lblGastos.Text = gastos.ToString();
+            lblSaldo.Text = saldo.ToString();
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtRenda.Clear();
+            txtEnergia.Clear();
+            txtInter.Clear();
+            txtAlimento.Clear();
+            txtOutros.Clear();
+            txtAgua.Clear();
+
+            lblGastos.Text = "0";
+            lblSaldo.Text = "0";
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+    }
+}
